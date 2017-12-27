927e124877ec4bcdfa14f9c6e253e6fa41d4de92
naughtyStark <sidharthtalia@ymail.com>
2017-12-27 18:22:08 +0530

    Update math_functions.ino

--- waypoint_follower/math_functions.ino
+++ waypoint_follower/math_functions.ino
@@ -1,12 +1,12 @@
 
-
 inline float anglecalcy(float x1,float x2,float y1,float y2)  //everything is inline because fuck you thats why
 {
-  if(atan2((y2-y1),(x2-x1))<0)
+  float angle = 57.3*atan2((y2-y1),(x2-x1));
+  if(angle<0)
   {
-    return 360 + ( 57.3*atan2((y2-y1),(x2-x1)) );
+    angle += 360;
   }
-  return 57.3*atan2((y2-y1),(x2-x1));    //angle that the line makes with EW axis 
+  return angle;
 }
 
 inline float distancecalcy(float y1,float y2,float x1,float x2,int i)
@@ -29,7 +29,7 @@ inline float mod(float a)  //taking mod of a number
 
 inline float gpsOpFlowKalman(float gpscord,float gpsError,float estimate,uint8_t trustInEstimate)   //used in localization tab
 {
-  estimateError=float(30.0/trustInEstimate);     //biasing was changed to 30 on 1/4/17
+  estimateError=(30.0/float(trustInEstimate));     //biasing was changed to 30 on 1/4/17 (not the american date standard.)
   estimateError*=estimateError; //squaring the estimate error because i want the trust to rise very quickly if surface quality is good and fall very quickly if it is lower than 20)
   KG=(estimateError/(estimateError+gpsError));
   return (KG*gpscord+(1-KG)*estimate);  
