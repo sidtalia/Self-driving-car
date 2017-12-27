ecc4146000393b5440369b223c3e4233af765012
naughtyStark <sidharthtalia@ymail.com>
2017-12-27 18:29:06 +0530

    Update SonarModule.ino

--- waypoint_follower/SonarModule.ino
+++ waypoint_follower/SonarModule.ino
@@ -1,56 +1,69 @@
-int L=50,C=50,R=50; //starting with non zero default values
-int lastC=50;
-#define ROOT3 1.732
-#define THRESHOLD 200
-#define BrakeP 1
-#define BrakeD 1
+//the reason why I am writing my own code to write the PWM instead of using the servo library is because 
+//It is not capable of writing the signals simultaneously, the signals go one after another. That method would have a worst case
+// time of 2*number of servos/esc's which is not a good thing if for example i intend to use separate in wheel motors to control the speed and direction.
 
+long esc_timer,end_timer;
+long throttle,steering;
+#define maxValue 2000
+#define minValue 1000
+#define STEERINGNULL 1480
+#define THROTTLENULL 1640
 
-void rebootSensors(){
-  digitalWrite(MAXSONAR_VCC, LOW);                // Pull it low before starting up
-  delay(10);                           // A little delay
-  digitalWrite(MAXSONAR_VCC, HIGH);               // Power up the sensors.
-  delay(250);                           // The sensors take 250ms to boot up.
-
-  // Give an initial trigger and then enter high impedance state
-  pinMode(MAXSONAR_TRIGGER,OUTPUT);               // Set the trigger pin to be output.
-  digitalWrite(MAXSONAR_TRIGGER,HIGH);              // Trigger begins
-  delayMicroseconds(100);                     // Trigger should be greater than 20us and less than 48ms
-  digitalWrite(MAXSONAR_TRIGGER,LOW);               // Trigger ends
-  pinMode(MAXSONAR_TRIGGER,INPUT);                // INPUT mode is high impedance for AVR controllers.
-}
-
-
-
-int msToCm(int microseconds) //function to convert time into distance  
+inline void servoWrite()
 {
-  return microseconds/58; //launde ko physics ati hai
+  while(PORTD>=8)
+  {
+    end_timer = micros(); //note the time
+
+    if(end_timer>=throttle) //if time has exceeded or is equal to the value of throttle
+    {
+      PORTD &= MOTOR_PIN; //pull the corresponding pin low
+    }
+    if(end_timer>=steering)
+    {
+      PORTD &= STEERING_PIN;
+    }
+  }
 }
 
-int read_Distance(int prev,int num)
+
+inline int limiter(int input) // prevent the values from going outside the 2000-1000us range
 {
-  int D;
-  D= (9*D + msToCm(input[num]) )/10; 
-  D = D>THRESHOLD ? THRESHOLD : D ;
-  return D;
+  if(input>maxValue)
+  {
+    return maxValue;
+  }
+  if(input<minValue)
+  {
+    return minValue;
+  }
+  return input;
 }
 
-
-void transferData()
+inline void driver() // function to operate the servo and esc.
 {
-  L= read_Distance(L,0);
-  
-  lastC=C;  //because braking has a PD system
-  C= read_Distance(C,1);
-  
-  R= read_Distance(R,2);
+  esc_timer = micros();//note the time 
+  PORTD |= PULL_HIGH; //pull the pins corresponding to the esc and servo high
+  if(d<2)
+  {
+    point++;
+    if(point>=4)
+    {
+      throttle = 1400 + esc_timer ;  //if within 1 meter of target, stahp
+    }
+    else
+    {
+      destY=c[point].retYO();
+      destX=c[point].retXO();
+    }
+  }
+  else
+  {
+    throttle = (THROTTLENULL + 50 - retard) + esc_timer;
+  }
+  steering = limiter(STEERINGNULL + 16*correction - yawRate) + esc_timer; //PD control on the steering.
+  servoWrite();
 }
 
-inline void callSonar()
-{
-  transferData();
-  sonarCorrection = depress( 57.3*atan(ROOT3*(L-R)/(min(L,R))),100);
-  retard = BrakeP*(THRESHOLD-C)+ BrakeD*(lastC-C);  
-}
 
 
