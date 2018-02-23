eeebd4c8e83f7fc5d3006ce3746a0b6ab65bd39b
naughtyStark <sidharthtalia@ymail.com>
2018-02-23 19:01:44 +0530

    Update wpf2.0.ino

--- wpf2.0.ino
+++ wpf2.0.ino
@@ -4,28 +4,29 @@
  *  3-throttle servo
  *  4-steering servo
  *  9,10,11,12,13- used for SPI by optical flow
+ *  A0 - r/c receiver, A1 ->USR multiplexing.
  *  free pins- 2,5,6,7,8.
 */
 
 /*NOTE: 
  * It would appear to you that a lot of the variables are global even though most would consider such a practice as unsafe. 
  * However on closer inspection, you will notice that the variables I have kept global are the ones that are shared between multiple functions.
- * This makes writing the functions easier for me, plus it reduces the function overhead as the variables are already in the working memory.
+ * This makes writing the functions etc easier for me, plus it reduces the function overhead as the variables are already in the working memory.
  * This also gives me a lot of freedom to use intermediate values from one function in another function(like accelerations, gyrations, etc) for filtering purposes.
  * It would be of some help if someone could tell me how i could make some variables read-only outside of a code block so that the system becomes a little more "secure"
  * without creating classes.
  */
  
 /* recent updates(latest first)-
- *  motorWrite has it's own separate tab
- *  driver module has it's own separate tab
+ * bezier curve based trajectory planning added.
+ * driver module has it's own separate tab
  * the function prototypes of separate tabs has been put into the main tab because the arduino IDE thinks i am writing in avr level and it wont let me define things in other tabs. 
  * usr module added 
  * miscellaneous changes- mh=90 initialization removed 
  *                       - global coordinates updated each cycle  
  *                       - math functions are in a separate tab 
  *                       - k(accel trust) was for some reason not being calculated, so added a statement for that
- * psuedo Kalman filter updated to handle gps and opflow instead of gps and imu.
+ * Kalman filter updated to handle gps and opflow instead of gps and imu.
  * i2c communication to slave arduinos added.function takes address of slave and the message array into which the message is stored. currently only for 2 pieces of information. Not in use right now
  * added optical flow code .  it's function is called in the localization tab before calling "callimu()". results from OpFlow are used in accelgyro tab 
  * magnetometer incorporated. function call is in accelgyro tab
@@ -51,8 +52,8 @@
 #include<Servo.h>
 
 Servo motor,steer;
-#define dt 0.0025 //cycle time
-
+#define dt 0.0025
+#define MAX_ACCELERATION 7.07 // max sideways acceleration is 10/root(2) m/s*s
 
 //--------------IMU STUFF-------------------------
 MPU9150 accelgyro;
@@ -62,20 +63,19 @@ uint8_t buf[6];
 float A[3],G[3],M[3],lastA[3],lastG[3];
 //413.97||764.10||16140.42||-134.85||94.52||162.61||
 
-float offsetA[3]={400,764,16176},offsetG[3]={-32,23,40},offsetM[3]={44174,33516,36478};//offsets for IMU
+float offsetA[3]={400,764,16176},offsetG[3]={-32,23,40},offsetM[3]={44174,33516,36478};//34816||40574||36478|
 float roll,pitch,yawRate;
-float magbuf[2]={0,0};//buffer for magnetic strength values
-float T[2]; //tilt(roll and pitch estimates using gyro
-float Ha,V=0; //horizontal acceleration, velocity estimate
+float magbuf[2]={0,0};
+float T[2];
+float Ha,V=0;
 //--------------IMU STUFF ENDS--------------------
 
 //--------------GPS STUFF-------------------------
 float latitude=0,longitude=0,Hdop=100000;
-float iLong,iLat,destLat,destLong;
-float destX,destY;
-bool tick=false;
+float iLong,iLat,lastX,lastY,lastLong,lastLat,destLat,destLong;
+bool tick=false; 
 #define GPSBAUD 115200
-#define MAXVARIANCE 20//max hdop in meters allowed during startup
+#define MAXVARIANCE 100//max hdop allowed during startup
 //--------------GPS STUFF ENDS--------------------
 
 //--------------OPTICAL FLOW STUFF----------------
@@ -120,20 +120,26 @@ int message[2];
 //--------------GENERAL VARIABLES-----------------
 float ml,mh; //ml=slope of line connecting bot to destination, mh= slope of line along heading.
 float d; // distance between bot and destination
-int l=1;  //l- stopping distance from target. tick- variable used to ensure that we don't add gps and imu values twice (look in localization tab)
-float globalX,globalY,X,Y;     //globalX and globalY are the final estimates of X and Y. theta is the angle rotated between consequent readings of gps,m is a variable for convenience 
-float correction=0;  //deviation required by the bot
+int l=1;  //l- stopping distance from target. 
+float globalX,globalY,X,Y; //globalX and globalY are the final estimates of X and Y. theta is the angle rotated between consequent readings of gps,m is a variable for convenience 
+float destX,destY,destSlope; //destination coordinates and heading.
+float int1[2],int2[2]; //intermediate points for the bezier curve
+float t; // fraction of the path at which the next ROC and Vmax is calculated. I hope I have not redefined it. One of the drawbacks of having a lot of global variables.
+float correction=0,Vmax,yaw_Compensation;  //steering angle required by the bot and max velocity of the bot.
 float now;  //variables for checking the time of the control loop
 float estimateError,KG;  //imu's error in estimation and kalman gain
 int point=1,i;
 bool accelRequired=false;
 float setPoint=1.5,error =0,Serror =0;
+int n;//number of coordinates
+int last_Throttle = 1500;// to keep a track of change in throttle
 //--------------GENERAL VARIABLES END-------------
 
 class coordinates //for storing multiple coordinates as we further progress to multiple waypoint trajectories instead of just 1 waypoint
 {                               //this will become the main point of focus once the basic tasks are complete
   float longitude,latitude,X,Y;
   public:
+  float slope;
   void setcords(float y,float x)
   {
     latitude=y;
@@ -160,10 +166,9 @@ class coordinates //for storing multiple coordinates as we further progress to m
   {
     return X;
   }
-  
 };
 
-coordinates c[4];
+coordinates c[8];
 
 void setup()
 {
@@ -223,8 +228,8 @@ void setup()
   steer.attach(4);
   for(int i=0;i<250;i++)
   {
-    motor.writeMicroseconds(1620);
-    steer.writeMicroseconds(1480);
+    motor.writeMicroseconds(1500);
+    steer.writeMicroseconds(1500);
     delay(20);
   }
   //----------------GENERAL SETUP BEGINS--------------
@@ -240,21 +245,38 @@ void setup()
       i--;
     }
   }
-  while(Hdop>MAXVARIANCE); //while HDOP>10
-  
+  while(Hdop>10000000); //while HDOP>10
+  lastLat = iLat;
+  lastLong = iLong;
+  //2/3 parking lot horizontal y/x
   c[0].setcords(iLat,iLong);  //STORE LOCATION OF ORIGIN IN C[0]
   //for now since i m working in a local frame, i can use meters instead of GPS coordinates, This is a little more reliable too as the GPS probably can't
   //tell me the exact initial location in degrees anyway.
   c[0].setcordsO(0.0,0.0);   //the location of origin is stored in an object of class coordinates using a member function called setcords(float x,float y)   
-  c[1].setcordsO(3.0,4.5);   //location of destination:-can be anything, is stored in another object of class coordinates
-  c[2].setcordsO(6.0,9.0);
-  c[3].setcordsO(9.0,13.5); //setting the final location as initial location to check error.
+  c[1].setcordsO(4.596,3.857);   //location of destination:-can be anything, is stored in another object of class coordinates
+  c[2].setcordsO(7.26,2.747);
+  c[3].setcordsO(1.832,-1.555); 
+  c[4].setcordsO(3,-7);
+  c[5].setcordsO(0.5,-8.5);
+  c[6].setcordsO(-2,-3.46);
+  c[7].setcordsO(0.0,0.0);//setting the final location as initial location to check error.
+//  c[1].setcordsO(1.932,0.517);
+//  c[2].setcordsO(2.449,-1.414);
+//  c[3].setcordsO(0.517,-1.932);
+//  c[4].setcordsO(0.0,0.0);
+  
   //setting the first destination coordinates.
+  X= lastX = globalX = 0;  //initializing globalX and globalY
+  Y= lastY = globalY = 0;
+  
+  c[0].slope = mh; //initial slope is the same as initial heading.
+  n = 8;
+  generate_Slopes();
+  c[7].slope = c[6].slope = mh; //defining it randomly for now.
+
   destX=c[1].retXO();// use (destLong-iLong)*111692.84 if working with degrees
   destY=c[1].retYO();//(destLat-iLat)*111692.84;
-     
-  X=globalX=0;  //initializing globalX and globalY
-  Y=globalY=0;
+  destSlope = c[1].slope;
   //--------------GENERAL SETUP ends-----------------------   
 
   
@@ -271,7 +293,7 @@ byte cycle = 1;
 void loop()
 {
   now = micros();
-  if(cycle==pow(2,0)||cycle==pow(2,4))
+  if(cycle==pow(2,0))
   {
     I2Cdev::writeByte(0x68,0x37,0x02); //ask MPU9150 to let me talk to the magnetometer.(XCL,XDA) 
     compute_All();//1000us
@@ -281,9 +303,26 @@ void loop()
       mh = 0.99*mh + 0.01*(tilt_Compensate(roll/57.3, pitch/57.3)); //tilt compensation takes angles in radians
     }//435us
     localization();//805
-  }//2240us plenty of time here
+    d=distancecalcy(globalY,destY,globalX,destX,0); //returns distance in meters. look into math_functions tab. 100us
+    t = get_T(V,d); //40us
+  }//2380us
+
+  if(cycle==pow(2,1))
+  {
+    compute_All(); //1000us
+    localization(); //805us
+    get_Intermediate_Points(mh,destSlope,globalX,destX,globalY,destY,d);//260us
+  }//2065us. some gap here
   
-  if(cycle==pow(2,3)||cycle==pow(2,7))
+  if(cycle==pow(2,2))
+  {
+    compute_All(); //1000us
+    localization(); //805us
+    Curvature(globalX,globalY,int1[0],int1[1],int2[0],int2[1],destX,destY,t,MAX_ACCELERATION);//585us. the change in globalX can't be more than 5cm even at 20m/s
+    //the function calculates the steering angle required and the max Velocity according to the max allowed lateral acceleration and yaw_Compensation
+  }//~2390us
+  
+  if(cycle==pow(2,3))
   {
     compute_All();//1000us
     I2Cdev::readBytes(0x0C, 0x03, 6, buf); //read the mag data
@@ -296,19 +335,13 @@ void loop()
       M[i] -= offsetM[i];
     } //135us
     localization();//805us
-    Compute_Steering_Distance(); //525us
   }//2465us
-  if(cycle==pow(2,1)||cycle==pow(2,5))
+  //lots of free time for the next 3 cycles. lets do something with it in future!
+  if(cycle==pow(2,4))
   {
     compute_All(); //1000us
-    localization(); //805us
-    Compute_Steering_Distance(); //525us
-  }//2330us some gap here
-  if(cycle==pow(2,2))
-  {
-    compute_All(); //1000us
-    localization(); //805us
-    if(a[0]==0&&a[1]==0&&a[2]==0&&g[0]==0&&g[1]==0&&g[2]==0) //12us to detect!
+    localization(); //805us  
+    if(!accelgyro.testConnection()) //12us to detect!
     {
       connection = false;
     }
@@ -316,15 +349,21 @@ void loop()
     {
       connection = true;
     }
-    
-  }//~1805us we got plenty of time in here
-  if(cycle==pow(2,6))
+  }
+  
+  if(cycle==pow(2,5)||cycle == pow(2,6))
+  {
+    compute_All(); //1000us
+    localization(); //805us  
+  }
+  
+  if(cycle==pow(2,7))
   {
     compute_All(); //1000us
     localization(); //805us
-    Compute_Steering_Distance(); //525us
-    driver();//100us
-  }//2430us
+    driver();
+  }//
+  
   cycle <<= 1;
   if(cycle==0)
   {
